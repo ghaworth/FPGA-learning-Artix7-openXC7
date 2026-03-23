0a18b3823267c2d63b36d02b95f7579e02075592
ghaworth <ghaworth66@googlemail.com>
2026-03-23 14:38:45 +0000

    Day 7: add capture module — snapshots beam state per row for VGA framebuffer

---
+++ projects/aoc_2025/day_07/day_07_capture.v
@@ -0,0 +1,25 @@
+module Day_07_Capture (
+	input wire clk,
+	input wire [2:0] state,
+    input wire [140:0] active,
+	output reg [6:0] address,
+	output reg [140:0] data,
+	output reg write_en = 0
+);
+
+	reg [2:0] prev_state;
+	wire row_done = (state == 1) && (prev_state != 1);
+	reg [6:0] row_count = 0;
+
+	always @(posedge clk) begin
+    prev_state <= state;
+    address <= row_count;
+    	if (row_done) begin
+    		data <= active;
+    		address <= row_count;
+    		row_count <= row_count + 1;
+    		write_en <= 1;
+    	end else begin
+    		write_en <= 0;
+    	end 
+    end
